43c450ca46ec8ec46152ee044df147800c65ae36
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-21 00:52:48 +0900

    Add question A file

---
+++ 第10回 アルゴリズム実技検定 過去問/A - 3枚のカード.py
@@ -0,0 +1,23 @@
+"""
+実行時間制限: 2 sec / メモリ制限: 1024 MB
+配点 : 9 点
+
+問題文
+3 枚のカードがあります。 1 枚目のカードには整数 A が、2 枚目のカードには整数 B が、3 枚目のカードには整数 C が書かれています。
+3 枚のカードの中から 2 枚のカードを選ぶとき、選んだ 2 枚に書かれた整数の積としてあり得る最小値と最大値を出力して下さい。
+
+制約
+−100≤A,B,C≤100
+A,B,C は整数
+"""
+
+
+class Solution:
+    @staticmethod
+    def three_cards(a: int, b: int, c: int) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    pass
+    # print(Solution().three_cards())
