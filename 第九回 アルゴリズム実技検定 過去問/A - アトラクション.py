0c0b7efe442fd32a3b459dfa7bc763d10b28938a
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-24 02:33:33 +0900

    Add question A file

---
+++ 第九回 アルゴリズム実技検定 過去問/A - アトラクション.py
@@ -0,0 +1,28 @@
+"""
+実行時間制限: 2 sec / メモリ制限: 1024 MB
+配点 : 9 点
+
+問題文
+高橋君は遊園地に行きました。
+この遊園地のアトラクションに乗るには、以下の条件を全て満たさなければなりません。
+
+身長 H cm 以上
+体重 W kg 以下
+高橋君は身長 h cm、体重 w kg です。
+高橋君はアトラクションに乗ることができますか？
+
+制約
+100≤H,h≤200
+50≤W,w≤100
+入力は全て整数
+"""
+
+
+class Solution:
+    @staticmethod
+    def attraction(h: int, w: int, h_s: int, w_s: int) -> str:
+        pass
+
+
+if __name__ == '__main__':
+    pass
