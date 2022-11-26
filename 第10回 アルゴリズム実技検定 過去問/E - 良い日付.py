bdc73d7757a2f17f5cda7e29da28af3e47ed3d0a
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-11-26 15:36:29 +0900

    Add question E file

---
+++ 第10回 アルゴリズム実技検定 過去問/E - 良い日付.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+
+class Solution:
+    @staticmethod
+    def good_day(s: str) -> str:
+        pass
+
+
+if __name__ == '__main__':
+    S = str(input())
+    print(Solution().good_day(S))
