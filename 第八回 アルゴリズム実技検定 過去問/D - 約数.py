84dabf96817814fe57f764ccc8ed13adf4ccf28e
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-02 00:27:07 +0900

    Finish question D

--- 第八回 アルゴリズム実技検定 過去問/D - 約数.py
+++ 第八回 アルゴリズム実技検定 過去問/D - 約数.py
@@ -13,14 +13,24 @@ Y の正の約数の個数の方が多いなら Y と出力し、
 1≤X,Y≤10^6
 X,Y は整数
 """
+from math import sqrt
 
 
 class Solution:
     @staticmethod
-    def divisor(x: int, y: int) -> str:
-        pass
+    def count_divisors(n: int) -> int:
+        return len({sub for i in range(1, int(sqrt(n)) + 1) for sub in (i, n // i) if n % i == 0})
+
+    @staticmethod
+    def compare_divisor(x: int, y: int) -> str:
+        if Solution.count_divisors(x) == Solution.count_divisors(y):
+            return "Z"
+        elif Solution.count_divisors(x) > Solution.count_divisors(y):
+            return "X"
+        else:
+            return "Y"
 
 
 if __name__ == '__main__':
     X, Y = map(int, input().split())
-    print(Solution.divisor(X, Y))
+    print(Solution.compare_divisor(X, Y))
