a9346be85f33de28794c237a7ca2010d57d1ac0e
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-03 00:45:52 +0900

    Add question B file

---
+++ 第七回 アルゴリズム実技検定 過去問/B - 蒸気圧.py
@@ -0,0 +1,23 @@
+"""
+https://atcoder.jp/contests/past202107-open/tasks/past202107_b
+実行時間制限: 2 sec / メモリ制限: 1024 MB
+配点 : 8 点
+
+問題文
+A 個のボールと B 個の風船があります。 高橋君はボールの数が風船の数の C 倍以下になるまで 1 つずつボールを取り除いていきます。
+最終的に残ったボールの数を風船の数で割った値を求めてください。
+
+制約
+1≤A,B,C≤10^4
+A,B,C は整数
+"""
+
+
+class Solution:
+    @staticmethod
+    def vapor_pressure():
+        pass
+
+
+if __name__ == '__main__':
+    pass
