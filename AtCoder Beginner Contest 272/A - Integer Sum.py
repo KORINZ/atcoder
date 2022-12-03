8ecd3978a6d7c9565d4e6829d12947197086a2b7
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-04 01:38:43 +0900

    Add question A file

---
+++ AtCoder Beginner Contest 272/A - Integer Sum.py
@@ -0,0 +1,24 @@
+"""
+https://atcoder.jp/contests/abc272/tasks/abc272_a
+実行時間制限: 2 sec / メモリ制限: 1024 MB
+配点 : 100 点
+
+問題文
+N 個の整数 A_1,A_2,…,A_Nが与えられます。
+N 個の整数を合計した値を求めてください。
+
+制約
+1≤N≤100
+1≤A_i≤100
+入力はすべて整数
+"""
+
+
+class Solution:
+    @staticmethod
+    def integer_sum():
+        pass
+
+
+if __name__ == '__main__':
+    pass
