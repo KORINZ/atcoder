eeb505929d241dc88416817750fc26fcc2d30a4b
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-05 18:31:14 +0900

    Finish question B

--- ABCクラス/AtCoder Beginner Contest 266/B - Modulo Number.py
+++ ABCクラス/AtCoder Beginner Contest 266/B - Modulo Number.py
@@ -1,3 +1,15 @@
 """
 https://atcoder.jp/contests/abc266/tasks/abc266_b
 """
+
+
+class Solution:
+    @staticmethod
+    def find_modulo_number(n: int) -> int:
+        special_number = 998244353
+        return n % special_number
+
+
+if __name__ == '__main__':
+    N = int(input())
+    print(Solution.find_modulo_number(N))
