a32b8c86d2617eb020ec305d318acee09df77bb4
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-05 18:21:05 +0900

    Finish question A

--- ABCクラス/AtCoder Beginner Contest 266/A - Middle Letter.py
+++ ABCクラス/AtCoder Beginner Contest 266/A - Middle Letter.py
@@ -1,3 +1,14 @@
 """
 https://atcoder.jp/contests/abc266/tasks/abc266_a
 """
+
+
+class Solution:
+    @staticmethod
+    def find_middle_letter(s: str) -> str:
+        return s[len(s)//2]
+
+
+if __name__ == '__main__':
+    S = input()
+    print(Solution.find_middle_letter(S))
