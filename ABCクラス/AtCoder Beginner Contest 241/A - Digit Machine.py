437c4f7b7f15e48d81215f8003159058ccc626de
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-05 22:53:46 +0900

    Finish question A

--- ABCクラス/AtCoder Beginner Contest 241/A - Digit Machine.py
+++ ABCクラス/AtCoder Beginner Contest 241/A - Digit Machine.py
@@ -1,3 +1,15 @@
 """
 https://atcoder.jp/contests/abc241/tasks/abc241_a
 """
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def use_digital_machine(a: List[int], curr_num: int = 0) -> int:
+        return a[a[a[curr_num]]]
+
+
+if __name__ == '__main__':
+    A = [int(i) for i in input().split()]
+    print(Solution.use_digital_machine(A))
