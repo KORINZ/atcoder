a02acc71f1001d86015e403834997e475708f470
KORINZ <111611023+KORINZ@users.noreply.github.com>
2022-12-05 23:32:34 +0900

    Update coding approach

--- ABCクラス/AtCoder Beginner Contest 241/B - Pasta.py
+++ ABCクラス/AtCoder Beginner Contest 241/B - Pasta.py
@@ -2,30 +2,22 @@
 https://atcoder.jp/contests/abc241/tasks/abc241_b
 """
 from typing import List
-from copy import deepcopy
 
 
 class Solution:
     @staticmethod
-    def can_finish_pasta(nums: int, days: int, storage: List[int], plan: List[int]) -> str:
+    def can_finish_pasta(days: int, storage: List[int], plan: List[int]) -> str:
 
-        plan_copy = deepcopy(plan)
-
-        if nums < days:
-            return 'No'
-
-        for i in plan_copy:
-            if i in storage:
-                plan.pop(plan.index(i))
-                storage.pop(storage.index(i))
-        if not plan:
-            return 'Yes'
-        else:
-            return 'No'
+        for i in range(days):
+            if plan[i] in storage:
+                storage.remove(plan[i])
+            else:
+                return 'No'
+        return 'Yes'
 
 
 if __name__ == '__main__':
     N, M = map(int, input().split())
     A = [int(i) for i in input().split()]
     B = [int(i) for i in input().split()]
-    print(Solution.can_finish_pasta(N, M, A, B))
+    print(Solution.can_finish_pasta(M, A, B))
